9f94159ad6ab255cc8c75682b847106b26b34b5b
er1c <ericzhao96@hotmail.com>
2021-05-27 23:29:16 +0800

    [add] stash

---
+++ 222.完全二叉树的节点个数.rb
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=222 lang=ruby
+#
+# [222] 完全二叉树的节点个数
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def count_nodes(root)
+
+end
+# @lc code=end
+
