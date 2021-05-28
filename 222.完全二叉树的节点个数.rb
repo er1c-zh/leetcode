8a5e77b059bd704e201787dcec732ddaa1f9f484
eric <ericzhao96@hotmail.com>
2021-05-28 14:58:29 +0800

    [add] 222

--- 222.完全二叉树的节点个数.rb
+++ 222.完全二叉树的节点个数.rb
@@ -17,7 +17,47 @@
 # @param {TreeNode} root
 # @return {Integer}
 def count_nodes(root)
-
+  return helper(root, -1, -1)
 end
-# @lc code=end
 
+def helper(root, l, r)
+  if l == -1
+    l = getl root
+  end
+  if r == -1
+    r = getr root
+  end
+
+  result = 0
+  if l == r
+    for i in 0...r
+      result += 2 ** i
+    end
+  else
+    result = 1 + (helper root.left, l - 1, -1) + (helper root.right, -1, r - 1)
+  end
+
+#   p "#{l}, #{r}, #{result}"
+
+  return result
+end
+
+def getl(root)
+  l = 0
+  while root != nil
+    l += 1
+    root = root.left
+  end
+  return l
+end
+
+def getr(root)
+  r = 0
+  while root != nil
+    r += 1
+    root = root.right
+  end
+  return r
+end
+
+# @lc code=end
