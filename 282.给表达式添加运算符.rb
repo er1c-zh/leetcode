9c3fb21dd0b9b1c47a64ab542e43dfaeaee781c0
eric <ericzhao96@hotmail.com>
2021-07-22 00:06:29 +0800

    [add] 282 ac

--- 282.给表达式添加运算符.rb
+++ 282.给表达式添加运算符.rb
@@ -10,10 +10,10 @@
 # @return {String[]}
 def add_operators(num, target)
     result = []
-    (1..num.length - 1).each_index{
+    (0..num.length - 1).each{
         |idx|
         s = num[0..idx]
-        result.concat helper([s.to_i], num[idx..-1], target, s.to_i)
+        result.concat helper([s.to_i], num[idx+1..-1], target, s.to_i)
         if num[0] == '0'
             break
         end
@@ -22,13 +22,14 @@ def add_operators(num, target)
 end
 
 def helper stack, num, target, cur
-    if s.length == 0
+    # puts "#{stack} #{num} #{target} #{cur}"
+    if num.length == 0
         return cur == target ? [stack.join] : []
     end
     result = []
     ['+', '-', '*'].each{
         |op|
-        (1..num.length - 1).each_index{
+        (0..num.length - 1).each{
             |idx|
             s = num[0..idx]
             nextVal = s.to_i
@@ -41,34 +42,39 @@ def helper stack, num, target, cur
             when '-'
                 _cur -= nextVal
             else '*'
-                preOp = ''
-                valFix = 0
+                valFix = 1
+                found = false
                 (0..stack.length - 1).each{
                     |delta|
-                    idx = stack.length - 1 - delta
-                    if ['-', '+'].include? stack[idx]
-                        if stack[idx] == '-'
+                    _idx = stack.length - 1 - delta
+                    if ['-', '+'].include? stack[_idx]
+                        found = true
+                        if stack[_idx] == '-'
                             _cur += valFix
-                            cur -= valFix * nextVal
+                            _cur -= valFix * nextVal
                         else
                             _cur -= valFix
-                            cur += valFix * nextVal
+                            _cur += valFix * nextVal
                         end
                         break
-                    elsif stack[idx] != '*'
-                        valFix += stack[idx] * 10
+                    elsif stack[_idx] != '*'
+                        valFix *= stack[_idx]
                     end
                 }
+                if !found
+                    _cur *= nextVal
+                end
             end
 
-
-            stack.push 'op' s.to_i
-
-
-
-            result.concat helper(stack, num[idx..-1], target, nextVal)
+            stack.push op
+            stack.push nextVal
+            result.concat helper(stack, num[idx + 1..-1], target, _cur)
             stack.pop
             stack.pop
+
+            if num[0] == '0'
+                break
+            end
         }
     }
     return result
@@ -81,9 +87,9 @@ end
 # @lc code=end
 
 if __FILE__ == $0
-    p add_operators "123", 6
+    #p add_operators "123", 6
     p add_operators "232", 8
-    p add_operators "105", 5
-    p add_operators "00", 0
-    p add_operators "3456237490", 9191
+    #p add_operators "105", 5
+    #p add_operators "00", 0
+    #p add_operators "3456237490", 9191
 end
