a62eb5ec3c5ff0a114fc6033327fc5dde796bd7e
eric <ericzhao96@hotmail.com>
2021-07-24 13:56:58 +0800

    [add] 287 binary search

--- 287.寻找重复数.rb
+++ 287.寻找重复数.rb
@@ -8,6 +8,31 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_duplicate(nums)
+    i, j = 1, nums.length
+    while true
+        mid = (i + j) / 2
+        l, r, e = 0, 0, 0
+        nums.each{
+            |v|
+            if v < mid
+                l += 1
+            elsif v == mid
+                e += 1
+            else
+                r += 1
+            end
+        }
+        if e > 1
+            return mid
+        end
+        if l < mid
+            i = mid
+        else
+            j = mid
+        end
+    end
+end
+def find_duplicate_floyd(nums)
     slow, fast = 0, 0
 
     step = 0
